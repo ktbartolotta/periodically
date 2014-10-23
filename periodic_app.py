a529ffdd95bc5d299e9326b09200017482d59548
Keif <mashidin@hotmail.com>
2014-10-23 19:40:50 -0400

    Moved the periodic table from a static txt to a list in periodic.py.

--- periodic_app.py
+++ periodic_app.py
@@ -9,37 +9,6 @@ app = Flask(__name__)
 @app.route('/<word>')
 def hello_world(word=""):
 
-    words2 = [
-        [
-            {
-                "name": "apple",
-                "atomic_no": 1,
-                "symbol": "Ap",
-                "atomic_wt": 2.132
-            },
-            {
-                "name": "banana",
-                "atomic_no": 2,
-                "symbol": "Bn",
-                "atomic_wt": 4.324
-            }
-        ],
-        [
-            {
-                "name": "Cactus",
-                "atomic_no": 3,
-                "symbol": "Ct",
-                "atomic_wt": 6.567
-            },
-            {
-                "name": "Dong",
-                "atomic_no": 4,
-                "symbol": "Dg",
-                "atomic_wt": 8.236
-            }
-        ]
-    ]
-
     words = periodic.get_periodics(word)
     return render_template("periodic.html", words=words)
 
