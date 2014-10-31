ebd62f4553e634d04eb0e8d63a13b53888a79874
Keif <mashidin@hotmail.com>
2014-10-30 20:05:58 -0400

    Moved styling to static css and added a static folder.

--- periodic_app.py
+++ periodic_app.py
@@ -5,18 +5,15 @@ import periodic
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index_page():
 
-    return render_template("base.html")
-
-
-@app.route("/add", methods=["POST"])
-def periodic_page():
-
-    word = request.form['word_entry']
-    words = periodic.get_periodics(word)
-    return render_template("periodic.html", words=words)
+    if request.method == "GET":
+        return render_template("base.html")
+    else:
+        word = request.form['word_entry']
+        words = periodic.get_periodics(word)
+        return render_template("periodic.html", words=words)
 
 if __name__ == "__main__":
     app.run(debug=True)
